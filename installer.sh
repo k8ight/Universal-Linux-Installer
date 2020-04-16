562b5fef09e1a0adb2edea7ee6bae30d3345b86e
Sounak Kar <skarunivarse@gmail.com>
2020-04-16 15:18:48 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -22,13 +22,14 @@ umount -f $diskname
 echo "Select partition scheme GPT or msdos (GPT is to be used for ufi & newer os| for MBR scheme on older os and windows  use msdos):"
 echo "select Partition Size[minimum 4GB required and mention GB after the number press enter for Full Drive install]:"
 read psize
+parted $diskname mklabel $psc
 if [ -z "$psize" ] then
-    $mksize="100%"
+    parted -a opt $diskname mkpart primary ext4  0% 100%
 else
-    $mksize="$psize"    
+  parted -a opt $diskname mkpart primary ext4  0% $psize  
 fi
-parted $diskname mklabel $psc 
-parted -a opt $diskname mkpart primary ext4  0% $mksize
+ 
+
 mkfs.ext4 -L debian $diskname"1"
 parted $diskname set 1 boot on
 mount $diskname"1" /mnt/ddrive 
